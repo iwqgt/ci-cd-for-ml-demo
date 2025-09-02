f610599f6118821b0112354e4a40032c75978bc1
Diego Contreras <diego.contreras@iwqgt.com>
2025-09-01 21:23:01 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,8 @@ def main():
 
 	#Get the current time and print it
 	current_time=datetime.datetime.now()
-	print("Current time:",current_time)
+	print("Current time2:",current_time)
 
 if __name__=='__main__':
+
 	main()
